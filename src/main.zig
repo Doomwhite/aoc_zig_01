10c48353a558de1f250d195b58956a0876e8fbf8
Adinelson Brühmüller <adinelson.bruhmuller@movtech.com.br>
2023-06-19 22:23:20 -0300

    WIP - why the fuck isn't the fmt working?????

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,23 @@
 const std = @import("std");
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    var file = try std.fs.cwd().openFile("src/input.txt", .{});
+    defer file.close();
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    var buf_reader = std.io.bufferedReader(file.reader());
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    var buf: [1024]u8 = undefined;
 
-    try bw.flush(); // don't forget to flush!
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+    var max: i32 = 0;
+    // var current_elf_calories: i32 = 0;
+    // var value: i32 = 0;
+    while (try buf_reader.reader().readUntilDelimiterOrEof(&buf, '\n')) |line| {
+        std.log.info("{s}", .{line});
+        const value = std.fmt.parseInt(i32, line, 10) catch -1;
+        if (value > max) {
+            max = value;
+        }
+        std.log.info("Value: {d}", .{value});
+    }
+    std.log.info("Max value: {d}", .{max});
 }
